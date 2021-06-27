eeyrjmr/openrazer

Add support for Razer BlackWidow v3 Pro

driver/razerkbd_driver.h
@@ -36,6 +36,9 @@
 #define USB_DEVICE_ID_RAZER_BLACKWIDOW_X_CHROMA 0x0216
 #define USB_DEVICE_ID_RAZER_BLACKWIDOW_X_ULTIMATE 0x0217
 #define USB_DEVICE_ID_RAZER_BLACKWIDOW_X_CHROMA_TE 0x021A
+#define USB_DEVICE_ID_RAZER_BLACKWIDOW_V3_PRO_USB 0x025A
+#define USB_DEVICE_ID_RAZER_BLACKWIDOW_V3_PRO_BLUETOOTH 0x025B
+#define USB_DEVICE_ID_RAZER_BLACKWIDOW_V3_PRO_WIFI 0x025C
 #define USB_DEVICE_ID_RAZER_ORNATA_CHROMA 0x021E
 #define USB_DEVICE_ID_RAZER_ORNATA 0x021F
 #define USB_DEVICE_ID_RAZER_BLADE_STEALTH_LATE_2016 0x0220

openrazer/razer-drivers

driver/razerkbd_driver.h
@@ -72,6 +72,9 @@
 #define USB_DEVICE_ID_RAZER_BLADE_EARLY_2020_BASE 0x0255
 #define USB_DEVICE_ID_RAZER_HUNTSMAN_MINI 0x0257
 #define USB_DEVICE_ID_RAZER_BLADE_STEALTH_LATE_2020 0x0259
+#define USB_DEVICE_ID_RAZER_BLACKWIDOW_V3_PRO_WIRED 0x025A
+#define USB_DEVICE_ID_RAZER_BLACKWIDOW_V3_PRO_BLUETOOTH 0x025B
+#define USB_DEVICE_ID_RAZER_BLACKWIDOW_V3_PRO_WIRELESS 0x025C
 #define USB_DEVICE_ID_RAZER_ORNATA_CHROMA_V2 0x025D
 #define USB_DEVICE_ID_RAZER_CYNOSA_V2 0x025E
 #define USB_DEVICE_ID_RAZER_BOOK_2020 0x026A